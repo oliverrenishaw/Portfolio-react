65ef60570f3c4d7f4134993d00a2783b761cec75
Oliver Warner <oliver.warner@renishaw.com>
2025-03-12 13:46:24 +0000

    feat: nav bar colours

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -33,7 +33,7 @@ const Header: React.FC = () => {
             )}
           </svg>
         </button>
-        <nav className="hidden sm:hidden md:flex bg-gradient-to-r from-[#ff9933] to-[#d5914e] h-12 justify-center items-center space-x-2 sm:space-x-4 p-1 sm:p-2 rounded-[25px]">
+        <nav className={`hidden sm:hidden md:flex h-12 justify-center items-center space-x-2 sm:space-x-4 p-1 sm:p-2 rounded-[25px] ${theme === 'dark' ? 'bg-gradient-to-r from-[#ff9933] to-[#d5914e]' : 'bg-gradient-to-r from-[#80bbc2] to-[#bbd7e1]'}`}>
           <a href="#Home" className="nav-link flex items-center justify-center px-1 py-1 sm:px-2 sm:py-1 md:px-4 md:py-2 text-black hover:text-white focus:text-white dark:text-white dark:hover:text-black dark:focus:text-black">Home</a>
           <a href="#About" className="nav-link flex items-center justify-center px-1 py-1 sm:px-2 sm:py-1 md:px-4 md:py-2 text-black hover:text-white focus:text-white dark:text-white dark:hover:text-black dark:focus:text-black">About</a>
           <a href="#Projects" className="nav-link flex items-center justify-center px-1 py-1 sm:px-2 sm:py-1 md:px-4 md:py-2 text-black hover:text-white focus:text-white dark:text-white dark:hover:text-black dark:focus:text-black">Projects</a>
@@ -61,7 +61,7 @@ const Header: React.FC = () => {
         leaveFrom="transform translate-y-0 opacity-100"
         leaveTo="transform -translate-y-full opacity-0"
       >
-        <nav className="sm:block md:hidden fixed top-16 left-0 w-full bg-gradient-to-b from-[#ff9933] to-[#e8a35f] flex flex-col items-start space-y-2 p-4 rounded-b-[25px] z-50">
+        <nav className={`sm:block md:hidden fixed top-16 left-0 w-full flex flex-col items-start space-y-2 p-4 rounded-b-[25px] z-50 ${theme === 'dark' ? 'bg-gradient-to-b from-[#ff9933] to-[#e8a35f]' : 'bg-gradient-to-b from-[#80bbc2] to-[#bbd7e1]'}`}>
           <a href="#Home" className="nav-link w-full text-center py-2 text-black hover:text-white focus:text-white dark:text-white dark:hover:text-black dark:focus:text-black" onClick={handleLinkClick}>Home</a>
           <a href="#About" className="nav-link w-full text-center py-2 text-black hover:text-white focus:text-white dark:text-white dark:hover:text-black dark:focus:text-black" onClick={handleLinkClick}>About</a>
           <a href="#Projects" className="nav-link w-full text-center py-2 text-black hover:text-white focus:text-white dark:text-white dark:hover:text-black dark:focus:text-black" onClick={handleLinkClick}>Projects</a>
